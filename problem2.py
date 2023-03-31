aa908701dbd50e8f452221413ab2ab7cf7e35726
Parneettiwana <Parneet.Kaur621@triosstudent.com>
2023-03-31 12:26:41 -0400

    v4

--- problem2.py
+++ problem2.py
@@ -1,7 +1,38 @@
-#define values
-cleaning_rate=60
-cavity_filling=200
-x_ray=100
-disc1=0.05
-disc2=0.1
-tax_rate=0.15
+def take_Input():
+    Number1 = int(input("Enter the first value: "))
+    Number2 = int(input("Enter the second value: "))
+#for addition
+    operator= input("enter the operand) (+):") 
+     return first value,second value,operator
+def declareResult():
+  first value, second value, operator = getinput()
+  if operand =="+":
+    result= first value + second value
+    print("{first value} + {second value} = {output}")
+    #for subtraction
+    operator= input("enter the operand) (-):") 
+   return first value,second value,operator
+def declareResult():
+  first value, second value, operator = getinput()
+  if operand =="-":
+    result= first value - second value
+    print(f"{first value} - {second value} = {output}")
+    #multiplication
+    operator= input("enter the operand) (*):") 
+   return first value,second value,operator
+def declareResult():
+  first value, second value, operator = getinput()
+  if operand =="*":
+    result= first value * second value
+    print(f"{first value} *{second value} = {output}")
+    #for division
+    operator= input("enter the operand) (/):") 
+   return first value,second value,operator
+def declareResult():
+  first value, second value, operator = getinput()
+  if operand =="/":
+    result= first value / second value
+    print(f"{first value} / {second value} = {output}")
+  else:
+    print("operator is invalid")
+    displayResult()
